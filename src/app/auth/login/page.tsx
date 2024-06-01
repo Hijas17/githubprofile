44fe0842e439820f03aa5a38cd4aa03f12ddf149
Hijas Abdul Salam <hijas.abdulsalam@i-o-tech.net>
2024-06-01 19:32:58 +0400

    loginpage changes

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,15 +1,22 @@
 import { cookies } from "next/headers";
 import React from "react";
 import "../../globals.css";
+import { GithubIcon } from "lucide-react";
 
 const page = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="flex flex-col items-center justify-center p-8 bg-white shadow-lg rounded-md">
+    <div className="flex items-center justify-center min-h-screen bg-[#0b323133] dark:bg-[#0b323133]">
+      <div className="flex flex-col items-center justify-center p-8 bg-[#0b323133] dark:bg-[#0b323133] shadow-lg rounded-md">
           <>
-            <h1 className="text-2xl font-bold text-center">Next.js GitHub OAuth</h1>
+          <div className="flex justify-center">
+          <div className=" bg-[#0b32318a] z-10 shadow-xl dark:bg-[#0b3231] rounded-full w-[5vw] h-[10vh]">
+          </div>
+          <div className="z-30 absolute p-1">
+          <GithubIcon className="mt-1" size={75}></GithubIcon>
+          </div>
+          </div>
             <form action="/api/auth/login" method="GET">
-              <button type="submit" className="px-4 py-2 mt-4 text-white bg-blue-500 rounded">
+              <button type="submit" className="px-4 py-2 mt-4 text-white bg-blue-800 rounded">
                 Login with GitHub
               </button>
             </form>
