cc30ed847ca2ecf4db9437f6434e0177fb8fc8cd
Hijas Abdul Salam <hijas.abdulsalam@i-o-tech.net>
2024-06-02 01:23:19 +0400

    env changes

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -2,9 +2,9 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   const clientId = process.env.GITHUB_CLIENT_ID;
-  const weburl = process.env.WEB_URL;
+  const WEB_URL = process.env.WEB_URL;
   const redirectUri = encodeURIComponent(
-    `https://${weburl}/api/auth/callback`
+    `${WEB_URL}/api/auth/callback`
   );
   const url = `https://github.com/login/oauth/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&scope=user`;
   console.log("url:", url);
